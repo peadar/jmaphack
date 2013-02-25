945fa4d0fb365c20f09637e355fbd3888d1b60c5
Peter Edwards <peter.edwards@vordel.com>
2013-02-25 17:36:41 +0000

    Actually commit the code for this hack.
    jmap doesn't work too well if the corefile being analyzed is greater
    than 2G in size, as it uses the "small" file open. This hack basically
    replaces the I/O routines in the standard library so that they take treat
    offsets as unsigned. This is not globally correct in any shape or form,
    but the usage pattern of jmap is such that it does actually work.

---
+++ lib.c
@@ -0,0 +1,42 @@
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <fcntl.h>
+
+typedef int (*openfn)(const char *, int, mode_t mode);
+typedef int (*preadfn)(int fd, void *buf, size_t bytes, off64_t off);
+
+openfn real_open;
+preadfn real_pread;
+
+
+static int inited;
+
+static void init()
+{
+    if (inited)
+        return;
+    inited = 1;
+    real_open = (openfn)dlsym(RTLD_NEXT, "open");
+    real_pread = (preadfn)dlsym(RTLD_NEXT, "pread64");
+    fprintf(stderr, "real open: %p, real pread: %p\n", real_open, real_pread);
+}
+
+int open(const char *file, int flags, ...)
+{
+    va_list args;
+
+    init();
+    va_start(args, flags);
+    mode_t m = va_arg(args, mode_t);
+    va_end(args);
+    return real_open(file, flags | O_LARGEFILE, m);
+}
+
+
+int pread(int fd, void *buf, size_t count, off_t off)
+{
+    init();
+    return real_pread(fd, buf, count, (unsigned long)off);
+}
+
